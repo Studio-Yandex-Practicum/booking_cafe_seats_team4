6d7b75b64c9d76f56e7b4a30bdf674e77c4a2c8f
IvanovArtem123 <akk0nom1@gmail.com>
2025-10-03 01:24:38 +0300

    fix style docstring

--- src/app/models/slots.py
+++ src/app/models/slots.py
@@ -3,6 +3,8 @@ from sqlalchemy import Column, DateTime, ForeignKey, Integer
 
 
 class Slots(Base):
+    """Информация о возможных интервалах бронирования."""
+
     cafe_id = Column(Integer, ForeignKey('cafes.id'))
     start_time = Column(DateTime)
     end_time = Column(DateTime)
