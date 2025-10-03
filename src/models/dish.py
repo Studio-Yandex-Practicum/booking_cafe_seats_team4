78fe9355f4098df6165816714f5db1c99d7b50e5
Daniil Pluzhnikov <daniil.pluzhnikov@mail.ru>
2025-10-03 22:31:41 +0200

    feat: добавлена модель Dish

---
+++ src/models/dish.py
@@ -0,0 +1,41 @@
+from datetime import datetime
+from decimal import Decimal
+
+from sqlalchemy import (
+    Boolean,
+    DateTime,
+    ForeignKey,
+    Numeric,
+    String,
+    Text,
+)
+from sqlalchemy.dialects.postgresql import UUID
+from sqlalchemy.orm import Mapped, mapped_column
+
+from src.models.base import Base
+
+
+class Dish(Base):
+    __tablename__ = "dishes"
+
+    id: Mapped[int] = mapped_column(primary_key=True)
+    cafe_id: Mapped[int] = mapped_column(
+        ForeignKey("cafes.id", ondelete="CASCADE"), nullable=False)
+
+    name: Mapped[str] = mapped_column(String(200), nullable=False)
+    description: Mapped[str | None] = mapped_column(Text)
+    price: Mapped[Decimal] = mapped_column(Numeric(10, 2), nullable=False)
+
+    is_available: Mapped[bool] = mapped_column(
+        Boolean, default=True, nullable=False)
+    media_id: Mapped[str | None] = mapped_column(UUID(as_uuid=True))
+
+    active: Mapped[bool] = mapped_column(Boolean, default=True, nullable=False)
+    created_at: Mapped[datetime] = mapped_column(
+        DateTime(timezone=True), default=datetime.utcnow
+    )
+    updated_at: Mapped[datetime] = mapped_column(
+        DateTime(timezone=True),
+        default=datetime.utcnow,
+        onupdate=datetime.utcnow
+    )
