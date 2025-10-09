76ed782b8773c0c22c9084ce48328bf891470097
Alexander Miroshnik <a.miro8256@gmail.com>
2025-10-09 21:19:14 +0300

    исправление стилистики.

--- src/core/db.py
+++ src/core/db.py
@@ -1,17 +1,16 @@
 from collections.abc import AsyncIterator
 from http import HTTPStatus
-from fastapi import HTTPException
-from sqlalchemy.ext.declarative import declarative_base
 
+from fastapi import HTTPException
 from sqlalchemy.ext.asyncio import (
     AsyncSession,
     async_sessionmaker,
     create_async_engine,
 )
+from sqlalchemy.ext.declarative import declarative_base
 
 from src.core.config import settings
 from src.core.logging import get_logger
-
 
 logger = get_logger(__name__)
 
@@ -44,7 +43,7 @@ async def get_session() -> AsyncIterator[AsyncSession]:
             await session.rollback()
             raise HTTPException(
                 status_code=HTTPStatus.INTERNAL_SERVER_ERROR,
-                detail=str(err)
+                detail=str(err),
             )
         finally:
             logger.info('Сессия закрыта.')
