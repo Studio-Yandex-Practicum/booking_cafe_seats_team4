59426b073e4a925a793711c06735b93b5fa2ff25
Mikhail Efimenko <meshok1589@yandex.com>
2025-10-24 03:26:08 +0300

    Fix flow

--- src/core/config.py
+++ src/core/config.py
@@ -35,14 +35,13 @@ class Settings:
     # Media
     MEDIA_PATH: Path = Path(os.getenv('MEDIA_PATH', '/media'))
 
-    # Celery & RabbitMQ
     CELERY_BROKER_URL: str = os.getenv(
         'CELERY_BROKER_URL',
-        'amqp://guest:guest@localhost:5672/',
+        'amqp://guest:guest@rabbitmq:5672/',
     )
     CELERY_RESULT_BACKEND: str = os.getenv(
         'CELERY_RESULT_BACKEND',
-        'redis://localhost:6379/0',
+        'redis://redis:6379/0',
     )
 
     # SMTP
