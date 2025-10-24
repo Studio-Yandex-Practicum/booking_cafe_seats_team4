5665a26c045e8f7d2a4c0d44edb03b13587394b3
Mikhail Efimenko <meshok1589@yandex.com>
2025-10-24 03:02:56 +0300

    Fix flow

--- src/celery_tasks/celery_app.py
+++ src/celery_tasks/celery_app.py
@@ -9,7 +9,6 @@ celery_app = Celery(
     'media_processor',
     broker=settings.CELERY_BROKER_URL,
     backend=settings.CELERY_RESULT_BACKEND,
-    include=['celery_tasks.tasks'],
 )
 celery_app.conf.update(
     task_serializer='json',
@@ -21,3 +20,5 @@ celery_app.conf.update(
     task_acks_late=True,
     task_reject_on_worker_lost=True,
 )
+
+import celery_tasks.tasks # noqa
